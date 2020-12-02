c0df07f6850055b0b984c6a90475275601a4c0e1
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2020-12-02 21:19:03 +0600

    form rendering to template(manualy)

--- FormApi/form/forms.py
+++ FormApi/form/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class UserCreateForm(forms.Form):
     first_name = forms.CharField(max_length=100, required=False)
     last_name = forms.CharField(max_length=100, initial="Tinne")
-    username = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100, widget=forms.HiddenInput)
     email = forms.CharField(max_length=100)
     password1 = forms.CharField(max_length=100, label='password')
     password2 = forms.CharField(max_length=100, label='Confirm Passwors')
--- FormApi/form/views.py
+++ FormApi/form/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . forms import UserCreateForm
 # Create your views here.
 def home(request):
-    form = UserCreateForm(label_suffix='', initial={'first_name':'Borhan', 'last-name':'setu'}, auto_id='saame')
+    form = UserCreateForm(label_suffix='', initial={'first_name':'Borhan', 'last-name':'setu'}, auto_id=True,)
+    form.order_fields(field_order=['username', 'first_name'])
     context = {'form':form}
     return render(request, 'home.html', context)
