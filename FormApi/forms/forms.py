427ed63f3984f31c0f48993a71e40efb10ddbf3a
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2020-12-15 07:43:48 +0600

    manualy form rendaring

--- FormApi/forms/forms.py
+++ FormApi/forms/forms.py
@@ -4,8 +4,8 @@ from django import forms
 class UserCreateForm(forms.Form):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100, help_text ='must be 6 charecrture')
     email = forms.EmailField(max_length=100)
     password = forms.CharField(max_length=100, required=False)
     
-    help_texts= {'username':'must be 6 charecrture'}
+    
