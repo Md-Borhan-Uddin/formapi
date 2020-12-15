d4649a53ca102a56828c78b999f40580282202f0
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2020-12-15 08:14:08 +0600

    for loop hidden fields

--- FormApi/forms/forms.py
+++ FormApi/forms/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class UserCreateForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, required=False,)
+    mobile = forms.IntegerField(  widget=forms.HiddenInput)
     last_name = forms.CharField(max_length=100)
     username = forms.CharField(max_length=100, help_text ='must be 6 charecrture')
     email = forms.EmailField(max_length=100)
