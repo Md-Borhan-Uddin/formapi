8ee92cba3dda683cc900f3b96b2645604b68c578
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2020-12-17 08:52:11 +0600

    errors display and style

--- FormApi/forms/forms.py
+++ FormApi/forms/forms.py
@@ -1,16 +1,29 @@
 from django import forms
 
 class UserCreateForm(forms.Form):
-    username = forms.CharField(min_length=7)
-    email = forms.EmailField(max_length=100)
-    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
-    password2 = forms.CharField(max_length=100, widget=forms.PasswordInput)
+    username = forms.CharField(min_length=7, error_messages={'required':'must 7 char'})
+    email = forms.EmailField(min_length=10)
+    password = forms.CharField()
+    password2 = forms.CharField()
     
+    
+    def clean_username(self):
+        name = self.cleaned_data.get('username')
+        if len(name) > 8:
+            raise forms.ValidationError('not valid name')
+        return name
+        
+    def clean_password(self):
+        pas = self.cleaned_data.get('password')
+        if len(pas) < 4:
+            raise forms.ValidationError('too short')
+        return pas
+        
+        
     def clean(self):
         cleaned_data = super().clean()
-        pas1 = self.cleaned_data['password']
-        pas2 = self.cleaned_data['password2']
-        
+        pas1 = self.cleaned_data.get('password')
+        pas2 = self.cleaned_data.get('password2')
         if pas1 != pas2:
             raise forms.ValidationError('password not match')
         
