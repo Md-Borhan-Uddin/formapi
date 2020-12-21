f25092ce18d1783a4166d957d14adb43558be388
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2020-12-21 07:22:25 +0600

    form data save into database

--- FormApi/forms/forms.py
+++ FormApi/forms/forms.py
@@ -1,30 +1,10 @@
 from django import forms
 
 class UserCreateForm(forms.Form):
-    username = forms.CharField(min_length=7, error_messages={'required':'must 7 char'})
+    username = forms.CharField(min_length=7)
     email = forms.EmailField(min_length=10)
-    password = forms.CharField()
-    password2 = forms.CharField()
-    
-    
-    def clean_username(self):
-        name = self.cleaned_data.get('username')
-        if len(name) > 8:
-            raise forms.ValidationError('not valid name')
-        return name
-        
-    def clean_password(self):
-        pas = self.cleaned_data.get('password')
-        if len(pas) < 4:
-            raise forms.ValidationError('too short')
-        return pas
-        
-        
-    def clean(self):
-        cleaned_data = super().clean()
-        pas1 = self.cleaned_data.get('password')
-        pas2 = self.cleaned_data.get('password2')
-        if pas1 != pas2:
-            raise forms.ValidationError('password not match')
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+ 
         
     
--- FormApi/forms/views.py
+++ FormApi/forms/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from .forms import UserCreateForm
-
+from . models import User
 # Create your views here.
 def formViews(request):
     if request.method == "POST":
         form = UserCreateForm(request.POST)
         if form.is_valid():
-            print('formvalidated')
             name = form.cleaned_data['username']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            print(name, email, password)
-            # print(form.cleaned_data['password'])
+            reg = User(username=name, emasil=email, password=password)
+            reg.save()
     else:        
         form = UserCreateForm(label_suffix=' ')
     context = {'form':form}
